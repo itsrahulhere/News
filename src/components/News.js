a97b9cf4929b65e1a95a240cf84318334b389007
Rahul <50038276+itsrahulhere@users.noreply.github.com>
2021-12-14 18:53:50 +0530

    Rahul

--- src/components/News.js
+++ src/components/News.js
@@ -60,7 +60,14 @@ const News = (props)=>{
                     <div className="row">
                         {articles.map((element) => {
                             return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                <NewsItem 
+                                title={element.title ? element.title : ""} 
+                                description={element.description ? element.description : ""} 
+                                imageUrl={element.urlToImage}
+                                newsUrl={element.url}
+                                author={element.author}
+                                date={element.publishedAt}
+                                source={element.source.name} />
                             </div>
                         })}
                     </div>
